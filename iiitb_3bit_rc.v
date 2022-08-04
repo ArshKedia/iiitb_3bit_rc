3522dc93e2a19dc7725d9defece58589bc072645
ArshKedia <64605104+ArshKedia@users.noreply.github.com>
2022-08-04 16:32:10 +0530

    Add files via upload

--- iiitb_3bit_rc.v
+++ iiitb_3bit_rc.v
@@ -3,16 +3,14 @@ module iiitb_3bit_rc(clk, ori, count);
 	input ori;
 	output[2:0] count;
 	reg[2:0] temp;
-	always @(posedge(clk), ori)
+	always @(posedge clk or posedge ori)
 	begin
 		if(ori == 1)
 		begin 
-			temp = 3'b100;
+			temp <= 3'b100;
 		end
-		else if(clk == 1)
-		begin
-			temp = {temp[0], temp[2:1]};
-		end
+		else
+			temp <= {temp[0], temp[2:1]};
 	end
 	assign count = temp;
 endmodule
--- yosys_run.sh
+++ yosys_run.sh
@@ -7,7 +7,9 @@ synth -top iiitb_3bit_rc
 
 # mapping to mycells.lib
 dfflibmap -liberty /home/arsh/IIITB/asic/iiitb_3bit_rc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+opt
 abc -liberty /home/arsh/IIITB/asic/iiitb_3bit_rc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+opt
 clean
 flatten
 # write synthesized design
