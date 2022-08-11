a7430ff6fd125bf2c3568d2a90fa759ad94bb30c
ArshKedia <64605104+ArshKedia@users.noreply.github.com>
2022-08-11 15:44:30 +0530

    Add files via upload

--- iiitb_3bit_rc_tb.v
+++ iiitb_3bit_rc_tb.v
@@ -2,12 +2,13 @@
 module iiitb_3bit_rc_tb;
 	reg clk;
 	reg ori;
-	wire[2:0] count;
+	wire [2:0] count;
 	iiitb_3bit_rc rc(clk, ori, count);
 	initial
 	begin
 		clk = 0;
-		ori = 0;
+		ori = 1;
+		#1000 $finish;
 	end
 	always
 	begin
@@ -18,8 +19,11 @@ module iiitb_3bit_rc_tb;
 	begin
 		$dumpfile("iiitb_3bit_rr_out.vcd");
 		$dumpvars(0, iiitb_3bit_rc_tb);
+		#20
+		ori = 0;
+		#200;
 		ori = 1;
-		#50;
+		#20
 		ori = 0;
 	end
 endmodule
